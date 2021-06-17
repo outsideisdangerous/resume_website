5a7a460253d4ed3bf642814436dec7455c28b9c4
outsideisdangerous <80612633+outsideisdangerous@users.noreply.github.com>
2021-06-18 00:17:19 +1000

    bacreview

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
+import {
+  FaReact,
+  FaHtml5,
+  FaCss3Alt,
+  FaPython,
+  FaLinkedin,
+  FaGithub,
+} from "react-icons/fa";
 import { workHistories, academics } from "./components/work";
+// import { ReactComponent as PythonLogo } from "./image/python-logo-inkscape.svg";
 
 function App() {
   const [tabIndex, setTabIndex] = useState(0);
@@ -33,62 +42,68 @@ function App() {
               src="https://images.pexels.com/photos/3109807/pexels-photo-3109807.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
               alt=""
             />
+            <div className="centered">Website of Jay Shin</div>
           </div>
         )}
         {tabIndex === 1 && (
-          <>
-            <header>
-              <h3>Languages and Libraries</h3>
-            </header>
-            <li>
-              <ul>
-                <h4> JavaScript + React Library</h4>
-                <p>useState</p>
-                <p>useEffect</p>
-                <p>Fetch, Context API</p>
-                <p>Routing</p>
-              </ul>
-              <ul>
-                <h4> HTML</h4>
-                <p>Tagging</p>
-                <p>Nesting</p>
-              </ul>
-              <ul>
-                <h4> CSS</h4>
-                <p>Styling Elements</p>
-                <p>Positioning Elements</p>
-                <p>Flexbox</p>
-                <p>Grid</p>
-                <p>Media Query</p>
-              </ul>
-              <ul>
-                <h4> Python</h4>
-              </ul>
-            </li>
-          </>
+          <div className="wrapper">
+            <div className="box">
+              <header>
+                <h3>Languages and Libraries</h3>
+              </header>
+              <li className="list-container">
+                <div>
+                  <FaReact className="react" />
+                  <h4> JavaScript + React</h4>
+                  <p>useState</p>
+                  <p>useEffect</p>
+                  <p>Fetch, Context API</p>
+                  <p>Routing</p>
+                </div>
+                <div>
+                  <FaHtml5 className="html" />
+                  <h4> HTML</h4>
+                  <p>Tagging</p>
+                  <p>Nesting</p>
+                </div>
+                <div>
+                  <FaCss3Alt className="css" />
+                  <h4> CSS</h4>
+                  <p>Styling Elements</p>
+                  <p>Positioning Elements</p>
+                  <p>Flexbox</p>
+                  <p>Grid</p>
+                  <p>Media Query</p>
+                </div>
+                <div>
+                  <FaPython className="python" />
+                  <h4> Python</h4>
+                </div>
+              </li>
+            </div>
+          </div>
         )}
         {tabIndex === 2 && (
-          <>
-            <header>
-              <h3>Contact</h3>
-            </header>
-            <li>
-              <ul>
-                <a
-                  className="urlink"
-                  href="https://github.com/outsideisdangerous"
-                >
-                  GitHub
-                </a>
-              </ul>
+          <div className="list-container-2">
+            <div className="motown">
+              <img src="https://i.imgur.com/NHI8bm0.jpg" alt="Jay" />
+            </div>
+            <h1 className="jay">Jay Shin</h1>
+            <p className="title">Pharmacist/Front-End Dev</p>
+            <p>University of South Australia</p>
 
-              <ul>
-                <a className="urlink" href="https://linkedin.com/in/jayshin2">
-                  LinkedIn
-                </a>
-              </ul>
-            </li>
-          </>
+            <a className="urlink" href="https://github.com/outsideisdangerous">
+              <FaGithub />
+            </a>
+            <a className="urlink" href="https://linkedin.com/in/jayshin2">
+              <FaLinkedin />
+            </a>
+            <a href="mailto:jaehanshin1@gmail.com">
+              <button className="contact-btn" type="submit">
+                Contact
+              </button>
+            </a>
+          </div>
         )}
       </main>
     </section>
